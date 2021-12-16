6ad6a67702ffc8fb80b9eea0f7a9304aa14bed70
jychen <jychen@bouffalolab.com>
2021-12-16 18:10:25 +0800

    update C++

--- note/handwritten digits/src/C++/handwritten_digits.cpp
+++ note/handwritten digits/src/C++/handwritten_digits.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include <random>
+#include <ctime>
+
+using Eigen::MatrixXd;
 
 class label_data {
 private:
@@ -151,14 +156,256 @@ public:
     }
 };
 
+void math_lib_test(void)
+{
+    MatrixXd a(2, 2);
+    MatrixXd b(2, 2);
+    MatrixXd c(2, 2);
+
+    a(0, 0) = 1;
+    b(0, 0) = 1;
+    a(0, 1) = 2;
+    b(0, 1) = 2;
+    a(1, 0) = 3;
+    b(1, 0) = 3;
+    a(1, 1) = 4;
+    b(1, 1) = 4;
+
+    c = a * b;
+
+    printf("[[%f,%f][%f,%f]] * [[%f,%f][%f,%f]] = [[%f,%f][%f,%f]\r\n]",
+           a(0, 0), a(0, 1), a(1, 0), a(1, 1),
+           b(0, 0), b(0, 1), b(1, 0), b(1, 1),
+           c(0, 0), c(0, 1), c(1, 0), c(1, 1));
+
+    c = a.transpose();
+
+    printf("[[%f,%f][%f,%f]].transpose = [[%f,%f][%f,%f]\r\n]",
+           a(0, 0), a(0, 1), a(1, 0), a(1, 1),
+           c(0, 0), c(0, 1), c(1, 0), c(1, 1));
+}
+
+class trainer {
+public:
+    uint32_t inode;
+    uint32_t hnode;
+    uint32_t onode;
+    double learnRate;
+
+    //result
+    MatrixXd wih;
+    MatrixXd who;
+
+    trainer(uint32_t _inode, uint32_t _hnode, uint32_t _onode, double _learnRate)
+    {
+        uint32_t i = 0, j = 0;
+        inode = _inode;
+        hnode = _hnode;
+        onode = _onode;
+        learnRate = _learnRate;
+
+        /* wih and who are trainer's outputs*/
+        /* create wih and who space */
+        wih = MatrixXd::Zero(hnode, inode);
+        who = MatrixXd::Zero(onode, hnode);
+
+        /* random initial w */
+        std::default_random_engine e(time(0));
+        std::normal_distribution<double> n(0, 0.1);
+
+        for (i = 0; i < (int)wih.rows(); i++) {
+            for (j = 0; j < (int)wih.cols(); j++) {
+                wih(i, j) = n(e);
+            }
+        }
+        for (i = 0; i < (int)who.rows(); i++) {
+            for (j = 0; j < (int)who.cols(); j++) {
+                who(i, j) = n(e);
+            }
+        }
+    }
+    void shape(MatrixXd mat)
+    {
+        printf("rows = %d , cols = %d", (int)mat.rows(), (int)mat.cols());
+    }
+    void look(MatrixXd mat)
+    {
+        int i = 0, j = 0;
+        for (i = 0; i < (int)mat.rows(); i++) {
+            printf("\n");
+            for (j = 0; j < (int)mat.cols(); j++) {
+                printf("%lf ", mat(i, j));
+            }
+        }
+    }
+
+    double sigmod_function(double x)
+    {
+        return 1.0 / (1 + exp(-x));
+    }
+
+    uint8_t forecast(uint8_t *data)
+    {
+        /* forward-propagating calculate error */
+        uint32_t i = 0, j = 0;
+
+        /* set *data into martix data structure */
+        MatrixXd dataM = MatrixXd::Zero(inode, 1);
+
+        for (i = 0; i < inode; i++) {
+            dataM(i, 0) = data[i];
+        }
+
+        MatrixXd a1 = MatrixXd::Zero(hnode, 1);
+        MatrixXd a2 = MatrixXd::Zero(onode, 1);
+        /* first layer propagating */
+        a1 = wih * dataM;
+
+        /* active */
+        for (i = 0; i < a1.rows(); i++) {
+            for (j = 0; j < a1.cols(); j++) {
+                a1(i, j) = sigmod_function(a1(i, j));
+            }
+        }
+
+        /* second layer propagating */
+        a2 = who * a1;
+
+        /* active */
+        for (i = 0; i < a2.rows(); i++) {
+            for (j = 0; j < a2.cols(); j++) {
+                a2(i, j) = sigmod_function(a2(i, j));
+            }
+        }
+    }
+
+    /* study a data */
+    double train(uint8_t *data, uint8_t *label)
+    {
+        double loss = 0;
+        /* forward-propagating calculate error */
+        uint32_t i = 0, j = 0;
+
+        /* set *data into martix data structure */
+        MatrixXd dataM = MatrixXd::Zero(inode, 1);
+
+        for (i = 0; i < inode; i++) {
+            dataM(i, 0) = data[i];
+        }
+
+        MatrixXd a1 = MatrixXd::Zero(hnode, 1);
+        MatrixXd a2 = MatrixXd::Zero(onode, 1);
+        /* first layer propagating */
+        a1 = wih * dataM;
+
+        /* active */
+        for (i = 0; i < a1.rows(); i++) {
+            for (j = 0; j < a1.cols(); j++) {
+                a1(i, j) = sigmod_function(a1(i, j));
+            }
+        }
+
+        /* second layer propagating */
+        a2 = who * a1;
+
+        /* active */
+        for (i = 0; i < a2.rows(); i++) {
+            for (j = 0; j < a2.cols(); j++) {
+                a2(i, j) = sigmod_function(a2(i, j));
+            }
+        }
+
+        /*BP*/
+
+        /* set *label into martix data structure */
+        MatrixXd labelM = MatrixXd::Zero(onode, 1);
+
+        /* set label format into output format */
+        labelM(*label, 0) = 1;
+
+        /*  layer 2 error */
+        MatrixXd delta2 = MatrixXd::Zero(onode, 1);
+
+        for (i = 0; i < delta2.rows(); i++) {
+            for (j = 0; j < delta2.cols(); j++) {
+                delta2(i, j) = labelM(i, j) - a2(i, j);
+                loss += delta2(i, j);
+            }
+        }
+
+        /*  layer 1 error */
+        MatrixXd delta1 = MatrixXd::Zero(hnode, 1);
+
+        delta1 = who.transpose() * delta2;
+
+        /* cal dwih */
+        MatrixXd dwih = MatrixXd::Zero(hnode, inode);
+        MatrixXd dwho = MatrixXd::Zero(onode, hnode);
+
+        for (i = 0; i < delta2.rows(); i++) {
+            for (j = 0; j < delta2.cols(); j++) {
+                delta2(i, j) = delta2(i, j) * a2(i, j) * (1 - a2(i, j));
+            }
+        }
+
+        dwho = delta2 * a1.transpose();
+
+        //cal dwho
+        for (i = 0; i < delta1.rows(); i++) {
+            for (j = 0; j < delta1.cols(); j++) {
+                delta1(i, j) = delta1(i, j) * a1(i, j) * (1 - a1(i, j));
+            }
+        }
+
+        dwih = delta2 * labelM.transpose();
+
+        //adjust wih and who
+        for (i = 0; i < dwih.rows(); i++) {
+            for (j = 0; j < dwih.cols(); j++) {
+                wih(i, j) = wih(i, j) + dwih(i, j) * learnRate;
+            }
+        }
+        for (i = 0; i < dwho.rows(); i++) {
+            for (j = 0; j < dwho.cols(); j++) {
+                who(i, j) = who(i, j) + dwho(i, j) * learnRate;
+            }
+        }
+        printf("%f,%f ", wih(0, 0), who(0, 0));
+        return loss;
+    }
+};
+
 int main()
 {
+    uint32_t i = 0, tim = 0;
+    uint32_t epochs = 5;
+
     image_data *train_image = new image_data("./train-images-idx3-ubyte");
     image_data *test_image = new image_data("./t10k-images-idx3-ubyte");
 
     label_data *train_label = new label_data("./train-labels-idx1-ubyte");
     label_data *test_label = new label_data("./t10k-labels-idx1-ubyte");
 
-    train_image->display_pic(0);
-    train_label->display_label(0);
+    //math_lib_test();
+
+    trainer *t = new trainer(28 * 28, 200, 10, 0.1);
+    for (tim = 0; tim < epochs; tim++) {
+        for (i = 0; i < train_image->image_number; i++) {
+            printf("loss=%lf %d/%d(%d)\r\n", t->train(train_image->get_pic(i), train_label->get_label(i)), i, train_image->image_number, tim);
+
+            //printf("%d/%d\r\n", i, train_image->image_number);
+        }
+    }
+
+    uint32_t right_num = 0;
+    for (i = 0; i < test_image->image_number; i++) {
+        if (t->forecast(test_image->get_pic(i)) == *test_label->get_label(i)) {
+            right_num++;
+            printf("c");
+        } else {
+            printf("e");
+        }
+    }
+
+    printf("\r\n score = %f\r\n", float(right_num) / (float)test_image->image_number);
 }
